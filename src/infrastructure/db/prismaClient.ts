26cd655b72043b1e4824462e56a6dec08d17d1a7
JC_from_zen <josec.backdev@gmail.com>
2026-03-25 10:01:28 -0500

    feat(prisma): correct client construct pattern
    Install adapter-pg ans it types to pass it to the prisma client contructor.

--- src/infrastructure/db/prismaClient.ts
+++ src/infrastructure/db/prismaClient.ts
@@ -1,12 +1,9 @@
 import "dotenv/config";
-import pg from "pg";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "@prisma/client";
 
-const pool = new pg.Pool({
+const adapter = new PrismaPg({
   connectionString: process.env["DATABASE_URL"],
 });
-
-const adapter = new PrismaPg(pool);
 
 export const prisma = new PrismaClient({ adapter });
